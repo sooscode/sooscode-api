0178212a0fb7484650a665a3f1ec17e4e894702a
sooxpak <sooxpak@gmail.com>
2025-12-16 15:01:26 +0900

    [feat]class: 수업모드 - 권한 분리

--- src/main/java/com/sooscode/sooscode_api/application/classroom/controller/ClassModeController.java
+++ src/main/java/com/sooscode/sooscode_api/application/classroom/controller/ClassModeController.java
@@ -1,9 +1,11 @@
 package com.sooscode.sooscode_api.application.classroom.controller;
 
+import com.sooscode.sooscode_api.application.classroom.dto.ClassModeMessage;
 import com.sooscode.sooscode_api.domain.classroom.enums.ClassMode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
@@ -18,9 +20,14 @@ public class ClassModeController {
      * 클라이언트가 /app/mode/{classId}로 메시지를 보내면
      * /topic/mode/{classId}를 구독한 모든 클라이언트에게 브로드캐스트
      */
-    @MessageMapping("/mode/{classId}")
-    public void changeMode(@DestinationVariable String classId, ClassMode classmode) {
-        // 해당 클래스의 모든 구독자에게 모드 변경 메시지 전송
-        messagingTemplate.convertAndSend("/topic/mode/" + classId, classmode);
+    @MessageMapping("/class/{classId}/mode")
+    public void changeMode(
+            @DestinationVariable Long classId,
+            ClassModeMessage message
+    ) {
+        messagingTemplate.convertAndSend(
+                "/topic/class/" + classId + "/mode",
+                message
+        );
     }
 }
---
+++ src/main/java/com/sooscode/sooscode_api/application/classroom/dto/ClassModeMessage.java
@@ -0,0 +1,10 @@
+package com.sooscode.sooscode_api.application.classroom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ClassModeMessage {
+    private String mode;
+}
